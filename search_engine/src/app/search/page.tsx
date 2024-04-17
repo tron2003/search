ba5422c351256140730e2f4da6a3c369c3cde582
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-04-17 17:18:43 +0530

    fix children problem son of bitch

--- search_engine/src/app/search/page.tsx
+++ search_engine/src/app/search/page.tsx
@@ -1,5 +1,29 @@
-const Page=()=>{
+// components/Page.tsx
+import { db } from "@/db";
+import { productsTable } from "@/db/schema";
+import { sql } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
-    return (<h1>hello worldasdacxvxcvdfsdsadasa</h1>)
+
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    };
 }
-export default Page
+
+const Page = async ({ searchParams }: PageProps) => {
+    const query = searchParams.query;
+    if (Array.isArray(query) || !query) {
+        return redirect("/");
+    }
+    let products = await db
+        .select()
+        .from(productsTable)
+        .where(
+            sql`to_tsvector('simple', lower(${productsTable.name} || ' ' || ${productsTable.description})) @@ to_tsquery('simple', lower(${query.trim().split(' ').join(' & ')}))`
+        )
+        .limit(3);
+    return <pre>{JSON.stringify(products)}</pre>;
+};
+
+export default Page;
---
+++ search_engine/src/db/index.ts
@@ -0,0 +1,7 @@
+// import { drizzle } from "drizzle-orm/mysql-proxy"
+
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
+
+const connector = neon(process.env.DATABASE_URL!)
+export const db = drizzle(connector)
